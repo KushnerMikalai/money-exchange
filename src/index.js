fe91e4855864ad4da66373d7f82f64045aef961f
Nikolai_Kushner <n.kushner@imedia.by>
2019-03-04 16:53:07 +0300

    feat: add solution makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+
+    const checkMoney = (currency) => {
+        let amount = {};
+        let next = currency;
+
+        const add = (num, rep, divider) => {
+            amount[rep] = Math.floor((num / divider));
+            next = next % divider;
+        };
+
+        if (next >= 50) {
+            add(next, 'H', 50);
+        }
+
+        if (next >= 25) {
+            add(next, 'Q', 25);
+        }
+
+        if (next >= 10) {
+            add(next, 'D', 10);
+        }
+
+        if (next >= 5) {
+            add(next, 'N', 5);
+        }
+
+        if (next >= 1) {
+            add(next, 'P', 1);
+        }
+
+        return amount;
+    };
+
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    } else if (currency === 0) {
+        return {};
+    } else {
+        return checkMoney(currency);
+    }
+};
